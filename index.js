590c8321018b09e5d0645db74fee28ff86223ec9
developerlin123 <developerlin123@gmail.com>
2021-10-22 10:50:33 +0200

    import all fake data

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ app.use(cors())
 // load router objs
 const Router = require('koa-router')
 let user=require('./serviceApi/user.js') 
-let home=require('./serviceApi/home.js') 
 let goods=require('./serviceApi/goods.js') 
 
 // mount all routes
 let router = new Router();
 router.use('/user',user.routes()) // all the routes will be under '/user/routes'
-router.use('/home',home.routes()) // ...
 router.use('/goods',goods.routes()) // ...
 // load router middlewares
 app.use(router.routes())
--- serviceApi/goods.js
+++ serviceApi/goods.js
@@ -18,14 +18,77 @@ router.get('/importgoods',async(ctx)=>{
             let newGoods = new Goods(value)
             newGoods.save().then(()=>{
                 saveCount++
-                console.log('Done'+saveCount)
+                console.log('Done:'+saveCount)
             }).catch(error=>{
-                 console.log('Save Failed'+error)
+                 console.log('Failed:'+error)
             })
         })
     })
-    ctx.body="import data..."
+    ctx.body="import goods"
 
+})
+
+router.get('/importcat',async(ctx)=>{
+    fs.readFile('./data_json/category.json','utf8',(err,data)=>{
+        data=JSON.parse(data)
+        let saveCount=0
+        const Category = mongoose.model('Category')
+        data.RECORDS.map((value,index)=>{
+            console.log(value)
+            let newCategory = new Category(value)
+            newCategory.save().then(()=>{
+                saveCount++
+                console.log('Done:'+saveCount)
+            }).catch(error=>{
+                 console.log('Failed:'+error)
+            })
+        })
+    })
+    ctx.body="import category"
+})
+
+router.get('/importsubcat',async(ctx)=>{
+    fs.readFile('./data_json/category_sub.json','utf8',(err,data)=>{
+        data = JSON.parse(data)
+        let saveCount = 0 
+        const CategorySub = mongoose.model('CategorySub')
+        data.RECORDS.map((value,index)=>{
+            console.log(value)
+            let newCategorySub = new CategorySub(value)
+            newCategorySub.save().then(()=>{
+                saveCount++
+                console.log('Done:'+saveCount)
+            }).catch(error=>{
+                console.log('Failed:'+error)
+            })
+        }) 
+    })
+    ctx.body="import sub_category"
+})
+
+//**get goods info */
+/* router.post('/getDetailGoodsInfo',async(ctx)=>{
+    let goodsId = ctx.request.body.goodsId
+    const Goods = mongoose.model('Goods')
+    await Goods.findOne({ID:goodsId}).exec()
+    .then(async(result)=>{  
+        ctx.body={code:200,message:result}
+    })
+    .catch(error=>{
+        console.log(error)
+        ctx.body={code:500,message:error}
+    })
+}) */
+
+router.post('/getDetailGoodsInfo',async(ctx)=>{
+    try{
+        let goodsId = ctx.request.body.goodsId
+        const Goods = mongoose.model('Goods')
+        let result=await Goods.findOne({ID:goodsId}).exec()
+        ctx.body={code:200,message:result} 
+    }catch(err){
+        ctx.body={code:500,message:err}
+    }
 })
 
 module.exports=router;
